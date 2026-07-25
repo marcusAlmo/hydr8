8708813a65cfbe01ab616fc7872eadb5956a4bd8
dashervx <mjalmojuela.dasher@gmail.com>
2026-07-25 11:21:20 +0800

    feat: implement index view and route for user application landing page

--- server/apps/users/views.py
+++ server/apps/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+    Renders the initial landing page for the application.
+    """
+    return render(request, 'users/index.html')
